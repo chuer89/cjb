67479d912698b4e53147a5537ea164b84e8bf49a
chu <chuer89@gmail.com>
2018-08-18 21:11:57 +0800

    up

--- app/src/routes/record/index.js
+++ app/src/routes/record/index.js
@@ -3,14 +3,37 @@ import React from 'react';
 import { connect } from 'dva';
 import App from '../app';
 import NProgress from 'nprogress';
-import { Table, Icon, Divider } from 'antd';
+import { Table, Icon, Divider, Button, Input, Select } from 'antd';
 import style from './record.less';
+
+const Option = Select.Option;
 
 class RecordList extends React.Component {
   constructor(props) {
     super(props);
     // 设置 initial state
     this.state = {
+      // 状态筛选
+      statusData: [{
+        value: '全部', code: '-1'
+      }, {
+        value: '离职', code: '1'
+      }, {
+        value: '在职', code: '2'
+      }, {
+        value: '待离职', code: '3'
+      }],
+
+      // 合同类型
+      contractType: [{
+        value: '全部', code: '-1'
+      }, {
+        value: '固定期限', code: '1'
+      }, {
+        value: '无固定期限', code: '2'
+      }, {
+        value: '试用', code: '3'
+      }]
     }
   }
 
@@ -21,12 +44,73 @@ class RecordList extends React.Component {
   render() {
     let { record } = this.props;
     let { columns, dataSource } = record;
-    
+    let inputStyle = {
+      'width': '180px',
+    }
+    let { statusData, contractType } = this.state;
+
+    let handleChange = (value) => {
+      console.log(value, 'v');
+    }
+
+    // 状态筛选
+    let renderSeleStatus = statusData.map((item) => {
+      return (
+        <Option value={item.code} key={item.code}>{item.value}</Option>
+      )
+    });
+
+    // 合同类型
+    let renderContractType = contractType.map((item) => {
+      return (
+        <Option value={item.code} key={item.code}>{item.value}</Option>
+      )
+    });
+
     return (
       <App>
         <div className={style.content}>
-        <h1>档案管理</h1>
-        <Table dataSource={dataSource} columns={columns} />
+          <div>
+            <Button type="primary" icon="user-add" style={{ 'marginRight': '15px' }}>添加员工</Button>
+            <Button type="primary" icon="usergroup-add">批量添加</Button>
+          </div>
+          <div className={style.searchBox}>
+            <div className={'clearfix'}>
+              <div className={style.searchItem}>
+                <span>工号：</span>
+                <Input placeholder="请输入工号" maxLength={32} style={inputStyle} />
+              </div>
+              <div className={style.searchItem}>
+                <span>姓名：</span>
+                <Input placeholder="请输入姓名" maxLength={32} style={inputStyle} />
+              </div>
+              <div className={style.searchItem}>
+                <span>状态：</span>
+                <Select defaultValue="全部" style={{ width: 120 }} onChange={handleChange}>
+                  {renderSeleStatus}
+                </Select>
+              </div>
+              <div className={style.searchItem}>
+                <span>合同类型：</span>
+                <Select defaultValue="全部" style={{ width: 120 }} onChange={handleChange}>
+                  {renderContractType}
+                </Select>
+              </div>
+            </div>
+            <div className={style.searchItemBox + ' clearfix'}>
+              <div className={style.searchItem}>
+                <span>合同类型：</span>
+                <Select defaultValue="全部" style={{ width: 120 }} onChange={handleChange}>
+                  {renderContractType}
+                </Select>
+              </div>
+              <div className={style.searchItem}>
+                <Button type="primary" style={{ 'marginRight': '15px' }}>查询</Button>
+                <Button>重置</Button>
+              </div>
+            </div>
+          </div>
+          <Table dataSource={dataSource} columns={columns} />
         </div>
       </App>
     );
--- app/src/routes/workbench/index.js
+++ app/src/routes/workbench/index.js
@@ -2,23 +2,43 @@ import React from 'react';
 import { connect } from 'dva';
 import App from '../app';
 import NProgress from 'nprogress';
+import { Card, Button, Tabs } from 'antd';
 
+const TabPane = Tabs.TabPane;
+
+// 工作台
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     // 设置 initial state
     this.state = {
+      loading: true,
     }
   }
 
   componentDidMount() {
+    let self = this;
     NProgress.done();
+    setTimeout(() => {
+      self.setState({
+        loading: false,
+      })
+    }, 2000)
   }
 
   render() {
+    let { loading } = this.state;
+
     return (
       <App>
         <h1>工作台</h1>
+        <Card loading={loading} title="人事概况">
+          <Tabs tabPosition={'left'}>
+            <TabPane tab="全部" key="1">Content of Tab 1</TabPane>
+            <TabPane tab="预警" key="2">Content of Tab 2</TabPane>
+            <TabPane tab="提醒" key="3">Content of Tab 3</TabPane>
+          </Tabs>
+        </Card>
       </App>
     );
   }
